8eaeb394080741335c419960dcee62f6faa1a2b1
Alvy <floressuarezalvaro@gmail.com>
2020-12-08 19:15:27 -0800

    added questions file

--- app.js
+++ app.js
@@ -6,13 +6,23 @@ const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const promptQs = require("./lib/promptQs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const promptQs = require("./lib/promptQs");
 
+let employees = [];
+let id = 0
 
+function inputEmp () {
+    id ++;
+    inquirer {
+        .prompt(promptQs)
+    }
+}
 
 
 
