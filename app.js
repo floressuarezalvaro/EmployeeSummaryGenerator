6bd2cd6fd396128f2c1f40fa46a69f37dbf24d4b
Alvy <floressuarezalvaro@gmail.com>
2020-12-09 21:52:15 -0800

    added output page, input new employees, and render main

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Employee = require("./lib/Employee");
 
 const inquirer = require("inquirer");
 const path = require("path");
@@ -21,13 +20,6 @@ function inputEmp () {
     inquirer
         .prompt(promptQs)
         .then((res) => {
-            console.log(res);
-            let titleRelatedQuestion = "";
-            let empName = "";
-            let empEmail = "";
-            let empTitle = "";
-            let empTitles = "";
-
             if (res.titleInput === "Manager"){
                 titleRelatedQuestion = "What is your office number?";
                 empName = res.nameInput;
@@ -53,7 +45,6 @@ function inputEmp () {
                     }
                 ])
                 .then((res2) => {
-                    console.log(res2);
                     empTitles = res2.titlesInput;
                 
                     switch (empTitle) {
@@ -71,6 +62,7 @@ function inputEmp () {
                     }
                     employeesArr.push(employee);
                     console.log(employeesArr);
+                    inputNewEmp()
                 })
                 .catch((error) => {
                     if (error) {console.log("You've encountered an error")}
@@ -78,30 +70,30 @@ function inputEmp () {
         });
 };
 
+function inputNewEmp() {
+    inquirer.prompt([
+    {
+        type: "confirm",
+        message: "Would you like to add another team member?",
+        name: "addTeammate",
+        default: true,
+    },
+    ]).then(addEmp => {
+    if (addEmp.addTeammate) {
+        inputEmp();
+    } else {
+        renderMain();
+    };
+    });
+};
+
+function renderMain () {
+    const HTML = render(employeesArr);
+    fs.writeFile(outputPath, HTML, (err) => {
+        if (err) throw err;
+        console.log("Wohoo, you created your page!")
+    })
+}
+
 inputEmp();
 
-
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
